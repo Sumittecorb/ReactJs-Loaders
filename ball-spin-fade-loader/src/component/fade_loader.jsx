7609374778e04cc1f46cd1950b74cf4f86fd5ea7
shail katiyar <shailvala.2020@tecorb.co>
2023-04-21 18:31:25 +0530

    implement ball spin fade loader

---
+++ ball-spin-fade-loader/src/component/fade_loader.jsx
@@ -0,0 +1,20 @@
+import "./loader.css";
+
+function FadeLoader() {
+  return (
+    <div className="main">
+    <div class="la-ball-spin-fade la-dark la-2x">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    </div>
+  );
+}
+
+export default FadeLoader;
